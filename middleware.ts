638a6358f91a757bcd3c16fd326ad83e6de4c52d
tta69 <tta.fiok@gmail.com>
2025-07-12 17:18:12 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,7 @@ import createMiddleware from 'next-intl/middleware';
 
 export default createMiddleware({
   locales: ['en', 'hu', 'ru'],
-  defaultLocale: 'en'
+  defaultLocale: 'ru'
 });
 
 export const config = {
