69db7959064ab0e874b58768a0a3d5a85dbcbbf9
tta69 <tta.fiok@gmail.com>
2025-07-12 13:36:43 +0200

    Create layout.tsx

---
+++ app/[locale]/layout.tsx
@@ -0,0 +1,22 @@
+// app/[locale]/layout.tsx
+import { NextIntlClientProvider, useMessages } from 'next-intl';
+import { ReactNode } from 'react';
+
+type Props = {
+  children: ReactNode;
+  params: { locale: string };
+};
+
+export default function LocaleLayout({ children, params: { locale } }: Props) {
+  const messages = useMessages();
+
+  return (
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          {children}
+        </NextIntlClientProvider>
+      </body>
+    </html>
+  );
+}
