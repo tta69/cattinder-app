c58a954d9217bc37d0567fe9e6b22236f1cf5701
tta69 <tta.fiok@gmail.com>
2025-07-12 21:40:26 +0200

    Update layout.tsx

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -1,14 +1,14 @@
 // app/[locale]/layout.tsx
 import { ReactNode } from 'react';
-import { NextIntlClientProvider, useMessages } from 'next-intl';
+import { NextIntlClientProvider, getMessages } from 'next-intl/server';
 
 type Props = {
   children: ReactNode;
   params: { locale: string };
 };
 
-export default function LocaleLayout({ children, params: { locale } }: Props) {
-  const messages = useMessages();
+export default async function LocaleLayout({ children, params: { locale } }: Props) {
+  const messages = await getMessages();
 
   return (
     <html lang={locale}>
