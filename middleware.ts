91dd104f9a1028ce88909562df5026854646e6c1
tta69 <tta.fiok@gmail.com>
2025-07-13 08:40:21 +0200

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,19 +1,10 @@
-import { NextRequest, NextResponse } from "next/server";
+import createMiddleware from 'next-intl/middleware';
 
-export function middleware(request: NextRequest) {
-  const pathname = request.nextUrl.pathname;
-
-  // Ha már lokalizált, akkor ne irányítsuk át
-  if (pathname.startsWith("/en") || pathname.startsWith("/hu") || pathname.startsWith("/ru")) {
-    return NextResponse.next();
-  }
-
-  // Átirányítás az alapértelmezett nyelvre (pl. en)
-  const url = request.nextUrl.clone();
-  url.pathname = `/en${pathname}`;
-  return NextResponse.redirect(url);
-}
+export default createMiddleware({
+  locales: ['en', 'hu', 'ru'],
+  defaultLocale: 'en'
+});
 
 export const config = {
-  matcher: ["/((?!_next|favicon.ico).*)"], // minden útvonalra, kivéve a statikus
+  matcher: ['/', '/(en|hu|ru)/:path*']
 };
