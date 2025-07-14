fe949be3f7f7c62b2c020aec36ba24b12dd04e8f
tta69 <tta.fiok@gmail.com>
2025-07-14 10:53:33 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,23 @@
-import './globals.css';
 import { ReactNode } from 'react';
-import Footer from '../components/Footer';
+import Footer from '../../components/Footer';
+import { NextIntlClientProvider } from 'next-intl';
+import { getMessages } from '../../lib/getMessages';
 
+export default async function LocaleLayout({
+  children,
+  params: { locale }
+}: {
+  children: ReactNode;
+  params: { locale: string };
+}) {
+  const messages = await getMessages(locale);
 
-export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
-      <body>{children}
+    <html lang={locale}>
+      <body>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          {children}
+        </NextIntlClientProvider>
         <Footer />
       </body>
     </html>
